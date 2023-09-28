c0fb1f01102c7a03f87a45be4d1f8a9c12077dac
craigAU854 <craigaustin021@gmail.com>
2023-09-28 23:32:33 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.querySelector('main#main');
+main.remove()
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+const yourName = 'Big man Ting';
+newHeader.textContent = '${yourName} is the champion'
+const body = document.querySelector('body');
+body.appendChild(newHeader);
